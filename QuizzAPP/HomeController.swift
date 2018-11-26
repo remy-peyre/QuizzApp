af9cd23995bc258fd5ae5c052864b25d22a029b8
sodaranh <sodara.nhek@supinternet.fr>
2018-11-26 17:03:56 +0100

    plist, login and alamofire work

--- QuizzAPP/HomeController.swift
+++ QuizzAPP/HomeController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class HomeController: UIViewController {
 
     @IBOutlet weak var buttonHome: UIButton!
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +23,32 @@ class HomeController: UIViewController {
     }
 
     @IBAction func buttonHome(_ sender: UIButton) {
-        print("HELLO")
-        self.performSegue(withIdentifier: "linkToHome", sender: nil)
+        print(emailField.text ?? "")
+        print(passwordField.text ?? "")
+        
+        /*self.performSegue(withIdentifier: "linkToHome", sender: nil)*/
+        login(email: emailField.text ?? "", password: passwordField.text ?? "")
+
     }
+    
+    func login(email: String, password: String) {
+        let head = ["Content-Type": "application/x-www-form-urlencoded"]
+        let param = ["email": email,"password": password]
+        
+        Alamofire.request("http://edu2.shareyourtime.fr/api/auth", method: .post, parameters: param, encoding: URLEncoding(), headers: head).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
